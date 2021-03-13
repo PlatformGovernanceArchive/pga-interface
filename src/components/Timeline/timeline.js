18dd9bc0e0737fccbec9912fa9b4bf25342292d7
crosssenses <lari_wu@gmx.de>
2021-03-13 23:52:00 +0100

    interactiv timeline module trial

--- src/components/Timeline/timeline.js
+++ src/components/Timeline/timeline.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import rd3 from 'react-d3-library';
+//import rd3 from 'react-d3-library';
+import InteractiveTimeline from 'react-interactive-timeline';
 
 // Importing bootstrap
 import Row from 'react-bootstrap/Row';
@@ -9,7 +10,7 @@ import Col from 'react-bootstrap/Col';
 //import node from './d3-timeline';
 //const RD3Component = rd3.Component;
 //const BarChart = rd3.BarChart;
-import * as d3 from 'd3';
+//import * as d3 from 'd3';
 
 class Timeline extends React.Component {
 
@@ -23,27 +24,27 @@ class Timeline extends React.Component {
 //  }
   render(){
 
-    useEffect(() => {
-      const svg = d3.select("#area");
-      svg
-        .append("circle")
-        .attr("cx", 50)
-        .attr("cy", 50)
-        .attr("r", 40)
-        .style("fill", "blue");
-      svg
-        .append("circle")
-        .attr("cx", 140)
-        .attr("cy", 70)
-        .attr("r", 40)
-        .style("fill", "red");
-      svg
-        .append("circle")
-        .attr("cx", 300)
-        .attr("cy", 100)
-        .attr("r", 40)
-        .style("fill", "green");
-    }, []);
+//    useEffect(() => {
+//      const svg = d3.select("#area");
+//      svg
+//        .append("circle")
+//        .attr("cx", 50)
+//        .attr("cy", 50)
+//        .attr("r", 40)
+//        .style("fill", "blue");
+//      svg
+//        .append("circle")
+//        .attr("cx", 140)
+//        .attr("cy", 70)
+//        .attr("r", 40)
+//        .style("fill", "red");
+//      svg
+//        .append("circle")
+//        .attr("cx", 300)
+//        .attr("cy", 100)
+//        .attr("r", 40)
+//        .style("fill", "green");
+//    }, []);
 
     const platforms = this.props.platforms
 
@@ -54,6 +55,8 @@ class Timeline extends React.Component {
 
       const selectedType = selectedPlatform.policies.filter(t => t.slug === this.props.type).pop()
 
+      console.log(selectedType)
+
       const date = this.props.date
 
       return (
@@ -61,10 +64,21 @@ class Timeline extends React.Component {
           <Col className="pageTitle">
             <h1>Timeline</h1>
             <p>I should show the timeline of <strong>{selectedPlatform.name}</strong> with the policy <strong>{selectedType.name}</strong> and the date <strong>{date}</strong> selected</p>
-            {/*<RD3Component data={this.state.d3} className="D3ComponentWrapper" />*/}
+            {/*<RD3Component data={this.state.d3} className="D3ComponentWrapper" />
             <div ref="timeline"></div>
             <button onClick={changeStroke}>change stroke</button>
-            <svg id="area" height={200} width={450}></svg>
+            <svg id="area" height={200} width={450}></svg>*/}
+            <InteractiveTimeline
+              startDate={'2018-09-01'}
+              endDate={'2019-06-30'}
+            >
+              <InteractiveTimeline.Row>
+                <InteractiveTimeline.Event date="2018-12-12" label="My event" />
+              </InteractiveTimeline.Row>
+              <InteractiveTimeline.Row fixedHeight>
+                <InteractiveTimeline.StepLabels />
+              </InteractiveTimeline.Row>
+            </InteractiveTimeline>
           </Col>
         </Row>
       )
