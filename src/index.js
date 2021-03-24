c709386f9f9896c31b87e5afaf0eced9497ea7d0
crosssenses <lari_wu@gmx.de>
2021-03-24 21:12:23 +0100

    Roll back

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
-import React,{ lazy, Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-//import { DataProvider } from './contexts/dataContext'
-
-const DataProvider = React.lazy(() => import('./contexts/dataContext')); // Lazy-loaded
+import { DataProvider } from './contexts/dataContext'
 
 ReactDOM.render(
-
-// Show a spinner while the profile is loading
-  <Suspense fallback={<p style={{heigth: 100+'vh'}}>Loading</p>}>
-    <DataProvider>
-      <App theme="darkTheme"/>
-    </DataProvider>
-  </Suspense>,
+  <DataProvider>
+    <App theme="darkTheme"/>
+  </DataProvider>,
   document.getElementById('root')
 );
